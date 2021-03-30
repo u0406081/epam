343b85a8c7ad3b0195eab178ecb6c5d83bb8f9d8
u0406081 <robotic56@yandex.ru>
2021-03-30 09:44:18 +0400

    lesson1

---
+++ src/main/java/vashetkov/lesson2/Cache.java
@@ -0,0 +1,100 @@
+package vashetkov.lesson2;
+
+/**
+ * Класс кэш.
+ * @autor 123
+ */
+public class Cache<T> {
+    private Object[] cache = null;
+    private int capacity;
+    private int size = 0;
+    Cache(int capacity){
+        this.capacity = capacity;
+        cache = new Object[capacity];
+    }
+
+    /**
+     * Добавляет
+     * @param element - элемент
+     */
+    public void add(T element){
+        if (size==capacity){
+            for (int j = 0; j < cache.length - 1; j++) {
+                cache[j] = cache[j + 1];
+            }
+            cache[cache.length-1] = element;
+        }else{
+            cache[size] = element;
+            size++;
+        }
+    }
+
+    /**
+     * Удаляет
+     * @param element - элемент
+     */
+    public void delete(T element) throws Exception {
+        Integer indexOfFinded = null;
+        for (int i=0; i < cache.length; i++) {
+            if (cache[i] != null && cache[i].equals(element)) {
+                indexOfFinded = i;
+                cache[i] = null;
+            }
+        }
+        if (indexOfFinded == null) throw new Exception("Element not found");
+        for (int i=indexOfFinded; i < cache.length-1; i++){
+            cache[i] = cache[i+1];
+            cache[i+1] = null;
+        }
+    }
+
+    /**
+     * Проверка на существование
+     * @param element - элемент
+     */
+    public boolean isPresent(T element){
+        Object o;
+        for (int i=0; i < cache.length; i++){
+            o = cache[i];
+            if (o != null && o.equals(element)) return true;
+        }
+        return false;
+    }
+
+
+    /**
+     * Достаёт объект
+     * @param element - элемент
+     */
+    public T get(T element){
+        Integer indexOfFinded = null;
+        T findedObject = null;
+        for (int i=0; i < cache.length; i++) {
+            if (cache[i] != null && cache[i].equals(element)) {
+                indexOfFinded = i;
+                findedObject = (T) cache[i];
+                cache[i] = null;
+            }
+        }
+        if (indexOfFinded == null) return null;
+        for (int i=indexOfFinded; i < cache.length-1; i++){
+            cache[i] = cache[i+1];
+            cache[i+1] = null;
+        }
+        cache[cache.length-1] = findedObject;
+        return  findedObject;
+    }
+
+    /**
+     * Чистит кэш
+     */
+    public void clear(){
+        for (int i=0; i < cache.length; i++){
+            cache[i] = null;
+        }
+    }
+
+    Object[] get(){
+        return cache;
+    }
+}
---
+++ src/main/java/vashetkov/lesson2/Main.java
@@ -0,0 +1,23 @@
+package vashetkov.lesson2;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        Cache<Integer> cache = new Cache(5);
+        cache.add(1);
+        cache.add(2);
+        cache.add(3);
+        cache.add(4);
+        cache.add(5);
+
+
+        //cache1.delete(2);
+        //System.out.println("isPresent = " + cache1.isPresent(6));
+        //cache.get(2);
+        //cache1.clear();
+
+        for (int i=0; i<10; i++) {
+            System.out.println(cache.get()[i]);
+        }
+    }
+}
