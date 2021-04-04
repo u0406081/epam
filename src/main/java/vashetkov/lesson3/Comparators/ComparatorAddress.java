8ef11c0c062f3bf747acedadeb55d8d8c40686b3
u0406081 <robotic56@yandex.ru>
2021-04-04 10:00:29 +0300

    Update ComparatorAddress.java

--- src/main/java/vashetkov/lesson3/Comparators/ComparatorAddress.java
+++ src/main/java/vashetkov/lesson3/Comparators/ComparatorAddress.java
@@ -5,7 +5,6 @@ import java.util.Comparator;
 
 public class ComparatorAddress implements Comparator<Human> {
 
-
     public int compare(Human h1, Human h2) {
         return h1.getAddress().toString().compareTo(h2.getAddress().toString());
     }
