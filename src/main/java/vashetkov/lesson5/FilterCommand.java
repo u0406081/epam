58f8577f8aec3e25a32452007a5cb23918218bf3
u0406081 <robotic56@yandex.ru>
2021-04-10 18:18:56 +0300

    Update FilterCommand.java

--- src/main/java/vashetkov/lesson5/FilterCommand.java
+++ src/main/java/vashetkov/lesson5/FilterCommand.java
@@ -1,15 +1,18 @@
-package vashetkov.lesson5;
-
 public class FilterCommand {
+
     public String filter(String line){
         String command = line.substring(0,line.indexOf(" "));
         String message = null;
+        AddCommand addCommand = new AddCommand();
+        DeleteCommand deleteCommand = new DeleteCommand();
+        PrintCommand printCommand = new PrintCommand();
+
         if ("add".equals(command))
-            message = AddCommand.addLineInFile(line);
+            message = addCommand.addLineInFile(line);
         else if ("delete".equals(command))
-            message = DeleteCommand.deleteLineInFile(line);
+            message = deleteCommand.deleteLineInFile(line);
         else if ("print".equals(command))
-            message = PrintCommand.printFileLine(line);
+            message = printCommand.printFileLine(line);
         return message;
     }
 }
